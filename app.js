79ef55f4b96bcec33d170bd2ed12a43c2b4c6e64
Melania Rizzuto <melrizzuto4@gmail.com>
2025-06-05 15:08:40 +0200

    fix: refactor and add surrender + isDisabled functions to disable buttons when game has a result

--- app.js
+++ app.js
@@ -13,61 +13,63 @@ const app = Vue.createApp({
             playerHealth: 100,
             currentRound: 0,
             winner: null
-        }
+        };
     },
     computed: {
-        monsterBar() {
-            return { width: this.monsterHealth + '%' }
-        },
-        playerBar() {
-            return { width: this.playerHealth + '%' }
+        isDisabled() {
+            return this.winner !== null;
         },
         useSuperAttack() {
             return this.currentRound % 3 !== 0;
+        },
+        monsterBar() {
+            return { width: Math.max(this.monsterHealth, 0) + '%' };
+        },
+        playerBar() {
+            return { width: Math.max(this.playerHealth, 0) + '%' };
         }
     },
     watch: {
         playerHealth(value) {
-            if (value <= 0 && this.monsterHealth < 0) {
-                this.winner = "draw"
+            if (value <= 0 && this.monsterHealth <= 0) {
+                this.winner = 'draw';
             } else if (value <= 0) {
-                this.winner = "monster"
+                this.winner = 'monster';
             }
         },
         monsterHealth(value) {
-            if (value <= 0 && this.playerHealth < 0) {
-                winner = "draw"
+            if (value <= 0 && this.playerHealth <= 0) {
+                this.winner = 'draw';
             } else if (value <= 0) {
-                this.winner = "player"
+                this.winner = 'player';
             }
         }
     },
     methods: {
         attackMonster() {
-            this.currentRound++
+            this.currentRound++;
             const attackValue = getRandomValue(5, 12);
             this.monsterHealth -= attackValue;
-            this.attackPlayer()
+            this.attackPlayer();
         },
         attackPlayer() {
-            const attackPlayer = getRandomValue(8, 15)
-            this.playerHealth -= attackPlayer
+            const attackValue = getRandomValue(8, 15);
+            this.playerHealth -= attackValue;
         },
         superDamage() {
-            this.currentRound++
-            const superDamage = getRandomValue(10, 30)
-            this.monsterHealth -= superDamage
-            this.attackPlayer()
+            this.currentRound++;
+            const damage = getRandomValue(10, 30);
+            this.monsterHealth -= damage;
+            this.attackPlayer();
         },
         toHealPlayer() {
-            this.currentRound++
-            const healValue = getRandomValue(8, 30)
-            if (this.playerHealth + healValue > 100) {
-                this.playerHealth === 100
-            } else {
-                this.playerHealth += healValue
-            }
-            this.attackPlayer()
+            this.currentRound++;
+            const heal = getRandomValue(8, 20);
+            this.playerHealth = Math.min(this.playerHealth + heal, 100);
+            this.attackPlayer();
+        },
+        surrender() {
+            this.winner = 'monster';
         }
     }
 })
