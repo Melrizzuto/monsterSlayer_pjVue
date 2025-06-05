e6bb478c55d11102d1a129b0a13fed24f515c806
Melania Rizzuto <melrizzuto4@gmail.com>
2025-06-05 12:27:53 +0200

    feat: implemented game result logic and conditional rendering

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const app = Vue.createApp({
             monsterHealth: 100,
             playerHealth: 100,
             currentRound: 0,
+            winner: null
         }
     },
     computed: {
@@ -23,6 +24,22 @@ const app = Vue.createApp({
         },
         useSuperAttack() {
             return this.currentRound % 3 !== 0;
+        }
+    },
+    watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth < 0) {
+                this.winner = "draw"
+            } else if (value <= 0) {
+                this.winner = "monster"
+            }
+        },
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth < 0) {
+                winner = "draw"
+            } else if (value <= 0) {
+                this.winner = "player"
+            }
         }
     },
     methods: {
